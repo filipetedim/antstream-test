b0484c7688ccafcc4098b9806780436c331a95f6
Filipe Tedim <filipetedim@gmail.com>
2021-09-12 16:17:17 +0100

    Added models for components

---
+++ api/models/achievement.js
@@ -0,0 +1,14 @@
+// Package dependencies
+const Mongoose = require('mongoose');
+
+// Schema
+const AchievementSchema = Mongoose.Schema({
+  parentComponent: { type: String, required: true },
+  name: { type: String, required: true },
+  sizeWidth: { type: Number, required: true },
+  sizeLength: { type: Number, required: true },
+  image: { type: String, required: false },
+});
+
+// Model
+module.exports = Mongoose.model('Achievement', AchievementSchema);
---
+++ api/models/game.js
@@ -0,0 +1,14 @@
+// Package dependencies
+const Mongoose = require('mongoose');
+
+// Schema
+const GameSchema = Mongoose.Schema({
+  parentComponent: { type: String, required: true },
+  name: { type: String, required: true },
+  sizeWidth: { type: Number, required: true },
+  sizeLength: { type: Number, required: true },
+  image: { type: String, required: false },
+});
+
+// Model
+module.exports = Mongoose.model('Game', GameSchema);
